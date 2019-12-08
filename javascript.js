497d52a5a98d50c9cd8a1bd8482c66302771a013
seanianking <seanianking@gmail.com>
2019-12-08 00:02:15 -0700

    datasets added, if statements added

--- javascript.js
+++ javascript.js
@@ -3,6 +3,21 @@ var upperEl = document.getElementById("uppercase");
 var lowerEl = document.getElementById("lowercase");
 var specialEl = document.getElementById("specials");
 var numberEl = document.getElementById("numbers");
+var submitEl = document.getElementById("generate");
+var copyEl = document.getElementById("copy");
+var passwordEl = document.getElementById("password");
+
+
+
+//password generate function
+passwordEl.addEventListener("click", function (){
+
+var passwordSelector = "";
+let length = parseInt(lengthEl.value);
+let lowerCase = lowerEl.checked;
+let upperCase = upperEl.checked;
+let specials = specialEl.checked;
+let numbers = numberEl.checked;
 
 //datasets to pull from
 var upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -10,3 +25,23 @@ var lowerSet = "abcdefghijklmnopqrstuvwxyz";
 var numbSet = "0123456789";
 var specSet = "`~!@#$%^&*()_-+=|}{[]';:/?><,.";
 
+//reading if boxes are checked, which characters will be used
+
+if (lowerCase){
+    passwordSelector += lowerSet
+};
+if (upperCase){
+    passwordSelector += upperSet
+};
+if (specials){
+    passwordSelector += specSet
+};
+if (numbers){
+    passwordSelector += numbSet
+};
+if (length < 8 || length > 100 ) {
+    alert("Password must be between 8 and 100 characters long");
+};
+passwordEl.innerText(length, passwordSelector);
+
+})

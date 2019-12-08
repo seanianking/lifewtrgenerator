98bbcd017b53f7b69371ddbba663f7acaf68808f
seanianking <seanianking@gmail.com>
2019-12-07 20:47:35 -0700

    declared variables in JS

---
+++ javascript.js
@@ -0,0 +1,12 @@
+var lengthEl = document.getElementById("length");
+var upperEl = document.getElementById("uppercase");
+var lowerEl = document.getElementById("lowercase");
+var specialEl = document.getElementById("specials");
+var numberEl = document.getElementById("numbers");
+
+//datasets to pull from
+var upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerSet = "abcdefghijklmnopqrstuvwxyz";
+var numbSet = "0123456789";
+var specSet = "`~!@#$%^&*()_-+=|}{[]';:/?><,.";
+
